399ee4adfc96667e1e9c3c1e41f1280e0aea542d
ILVIAN <ilvianfashionbrand@gmail.com>
2025-06-29 16:59:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function loopCarousel(id) {
+  const row = document.getElementById(id);
+  const clone = row.innerHTML;
+  row.innerHTML += clone;
+}
 
+loopCarousel('men-row');
+loopCarousel('women-row');
+loopCarousel('kids-row');
+loopCarousel('fragrance-row');
